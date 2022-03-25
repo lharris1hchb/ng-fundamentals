fdaeacd245b329839968916fba45bdaf15947a78
Lincoln Harris <lharris1@hchb.com>
2022-03-25 11:44:15 -0500

    angular template syntax

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -6,12 +6,19 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./event-thumbnail.component.css']
 })
 export class EventThumbnailComponent implements OnInit {
+  readonly earlyStartTime : string = '8:00 am';
+  readonly lateStartTime : string = '10:00 am';
 
   @Input() event: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getStartTimeClass() : any {
+    const isEarlyStart = this.event?.time === this.earlyStartTime;
+    return { green: isEarlyStart, bold: isEarlyStart };
   }
 
 }
