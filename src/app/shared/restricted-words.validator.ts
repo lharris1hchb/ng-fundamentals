12f77cb18c4160286b0d3ee6ef4cdce889925342
Lincoln Harris <lharris1@hchb.com>
2022-03-28 19:26:28 -0500

    actually fixing validator issue

--- src/app/shared/restricted-words.validator.ts
+++ src/app/shared/restricted-words.validator.ts
@@ -1,8 +1,7 @@
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { AbstractControl } from '@angular/forms';
 
   export function restrictedWordsValidator(words: string[] | null) {
-    return (control: FormControl) : {[key:string]: any} | null => {
+    return (control: AbstractControl) : {[key:string]: any} | null => {
       if(words?.length) {
         console.log(words);
         var invalidWords = words.filter(w => control.value.includes(w));
